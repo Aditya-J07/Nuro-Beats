da674761d0276e9e34d86b7abfa5849849a76c6e
Aditya Jha <adityajha.vats02@gmail.com>
2025-10-05 20:45:31 +0530

    Update api_main.py

--- api_main.py
+++ api_main.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("BACKEND_PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Render uses PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
